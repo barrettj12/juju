7d3f742f91d71c8018522768ddb9647c0960ce5a
Jordan Barrett <jordan.barrett@canonical.com>
2023-03-17 17:04:45 +1100

    WIP for Prometheus integration

--- cmd/jujud/agent/machine/manifolds.go
+++ cmd/jujud/agent/machine/manifolds.go
@@ -51,6 +51,7 @@ import (
 	"github.com/juju/juju/worker/changestream"
 	"github.com/juju/juju/worker/common"
 	lxdbroker "github.com/juju/juju/worker/containerbroker"
+	"github.com/juju/juju/worker/controllercharm"
 	"github.com/juju/juju/worker/controllerport"
 	"github.com/juju/juju/worker/credentialvalidator"
 	"github.com/juju/juju/worker/dbaccessor"
@@ -784,6 +785,15 @@ func commonManifolds(config ManifoldsConfig) dependency.Manifolds {
 				Logger:        loggo.GetLogger("juju.worker.secretbackendsrotate"),
 			},
 		))),
+
+		// TODO: use more ifX conditions here?
+		controllerCharmName: ifNotMigrating(ifPrimaryController(controllercharm.Manifold(controllercharm.ManifoldConfig{
+			APICallerName: apiCallerName,
+			Hub:           config.LocalHub,
+			Logger:        loggo.GetLogger("juju.worker.controllercharm"),
+			NewFacade:     controllercharm.NewFacade,
+			NewWorker:     controllercharm.NewWorker,
+		}))),
 	}
 
 	return manifolds
@@ -1157,4 +1167,6 @@ const (
 	brokerTrackerName = "broker-tracker"
 
 	charmhubHTTPClientName = "charmhub-http-client"
+
+	controllerCharmName = "controller-charm"
 )
